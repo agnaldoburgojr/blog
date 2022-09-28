4f92470f159de9f250a7736516a9e6af77757c0d
Agnaldo Burgo Junior <agnaldoburgojunior@MacBook-Air-de-Agnaldo.local>
2022-09-28 09:30:48 -0300

    feat: add comments to articles

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,8 @@
-class Article < ApplicationRecord
-  has_many :comments
-  
-  validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+class Comment < ApplicationRecord
+  belongs_to :article
 
   VALID_STATUSES = ['public', 'private', 'archived']
+
   validates :status, inclusion: { in: VALID_STATUSES }
 
   def archived?
